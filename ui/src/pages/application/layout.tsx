61a1f37d5ae445e7545372fd5d42fb3ab2916e41
Silvio <silvio.cesarrx@hotmail.com>
2025-07-22 23:24:37 -0300

    create app frame

--- ui/src/pages/application/layout.tsx
+++ ui/src/pages/application/layout.tsx
@@ -5,7 +5,7 @@ export default function AppLayout() {
   return (
     <div className="grid min-h-screen grid-rows-[auto_1fr_auto]">
       <header className="flex h-16 items-center justify-between px-8 py-4">
-        <h1 className="text-2xl">Nex Loyalty</h1>
+        <h1 className="text-2xl font-semibold">Nex Loyalty | Transações</h1>
         <ThemeSwitcher />
       </header>
 
@@ -13,7 +13,7 @@ export default function AppLayout() {
         <Outlet />
       </main>
 
-      <footer className="bg-foreground text-background flex items-center justify-between px-8 py-4">
+      <footer className="bg-foreground text-background flex items-center justify-between px-8 py-2">
         <p>Feito por Silvio Cesar</p>
         <p>Vaga FullStack Nex</p>
       </footer>
--- ui/src/pages/application/transactions/index.tsx
+++ ui/src/pages/application/transactions/index.tsx
@@ -1,14 +1,146 @@
 import { Button } from "@/components/ui/button";
 
+import { HighlightCards } from "@/components/HighlightCards";
+
+import { Input } from "@/components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { DatePicker } from "@/components/ui/date-picker";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+const invoices = [
+  {
+    invoice: "INV001",
+    paymentStatus: "Paid",
+    totalAmount: "$250.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV002",
+    paymentStatus: "Pending",
+    totalAmount: "$150.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV003",
+    paymentStatus: "Unpaid",
+    totalAmount: "$350.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV004",
+    paymentStatus: "Paid",
+    totalAmount: "$450.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV005",
+    paymentStatus: "Paid",
+    totalAmount: "$550.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV006",
+    paymentStatus: "Pending",
+    totalAmount: "$200.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV007",
+    paymentStatus: "Unpaid",
+    totalAmount: "$300.00",
+    paymentMethod: "Credit Card",
+  },
+];
+
 export default function Transactions() {
   return (
-    <div className="flex h-full">
-      <Button
-        className="cursor-pointer transition-all duration-300 ease-in-out"
-        onClick={() => console.log("Teste")}
-      >
-        Click me
-      </Button>
+    <div className="flex h-full flex-col gap-8">
+      <HighlightCards />
+
+      <div className="flex flex-col flex-wrap gap-4 sm:flex-row sm:items-center sm:justify-between">
+        <div className="flex flex-wrap items-center gap-2">
+          <Input type="text" placeholder="Search" className="w-fit" />
+          <DatePicker />
+          <Select>
+            <SelectTrigger className="w-[150px]">
+              <SelectValue placeholder="Status" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Status</SelectLabel>
+                <SelectItem value="approved">Aprovado</SelectItem>
+                <SelectItem value="rejected">Reprovado</SelectItem>
+                <SelectItem value="evaluation">Em Avaliação</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="flex flex-row flex-wrap items-center gap-2">
+          <Button
+            className="w-fit cursor-pointer transition-all duration-300 ease-in-out"
+            onClick={() => console.log("Teste")}
+          >
+            Click me
+          </Button>
+          <Button
+            className="w-fit cursor-pointer transition-all duration-300 ease-in-out"
+            onClick={() => console.log("Teste")}
+          >
+            Click me
+          </Button>
+        </div>
+      </div>
+
+      <div className="overflow-hidden rounded-md border">
+        <Table>
+          <TableCaption className="pb-4">
+            A list of your recent invoices.
+          </TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Invoice</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Method</TableHead>
+              <TableHead className="text-right">Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {invoices.map((invoice) => (
+              <TableRow key={invoice.invoice}>
+                <TableCell className="font-medium">{invoice.invoice}</TableCell>
+                <TableCell>{invoice.paymentStatus}</TableCell>
+                <TableCell>{invoice.paymentMethod}</TableCell>
+                <TableCell className="text-right">
+                  {invoice.totalAmount}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TableCell colSpan={3}>Total</TableCell>
+              <TableCell className="text-right">$2,500.00</TableCell>
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </div>
     </div>
   );
 }
